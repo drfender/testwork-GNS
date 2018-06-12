7b2110fd010d3f8374cbdd31374d9c3f36522856
Unknown <dr.fender17@gmail.com>
2018-06-12 19:13:50 +0300

    complete testwork

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,65 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld/>
+    <search/>
+    <table>
+      <headTable/>
+      <infoTable/>
+    </table>
+    <total/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
+  import infoTable from './components/infoTable'
+  import headTable from './components/headTable'
+  import total from "./components/total";
+  import search from './components/search'
+  import store from './store/index.js'
+  import {mapState, mapGetters, mapActions} from 'vuex';
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  export default {
+    name: 'App',
+    components: {
+      infoTable,
+      headTable,
+      total,
+      search
+    },
+    store: store,
+    computed: {
+      ...mapGetters(['users']),
+    },
+    methods: mapActions(['getUsers']),
+    created() {
+      this.getUsers({});
+    },
   }
-}
 </script>
-
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  body {
+    background: #20262E;
+    padding: 20px;
+    font-family: Helvetica;
+  }
+
+  #app {
+    background: #fff;
+    border-radius: 4px;
+    padding: 20px;
+    transition: all 0.2s;
+  }
+
+  table {
+    width: 100%;
+  }
+
+  form input {
+    display: block;
+  }
+
+  th {
+    text-align: left;
+  }
 </style>
+
+
